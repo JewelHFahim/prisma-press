f2e776202810e8a61a979a8fb11fa8e723742a5f
JewelHFahim <jewelhfahim@gmail.com>
2026-07-18 12:58:02 +0600

    accessToken & RefreshToken implemented

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,6 +1,9 @@
 import bcrypt from "bcryptjs";
 import { prisma } from "../../lib/prisma";
 import { ILoginUser } from "./auth.interface";
+import jwt, { SignOptions } from "jsonwebtoken";
+import config from "../../config";
+import { jwtUtils } from "../../utils/jwtUtils";
 
 const loginUser = async (payload: ILoginUser) => {
   const { email, password } = payload;
@@ -15,7 +18,26 @@ const loginUser = async (payload: ILoginUser) => {
     throw new Error("Password is invalid!");
   }
 
-  return user;
+  const jwtPayload = {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+  };
+
+  const accessToken = jwtUtils.creatToken(
+    jwtPayload,
+    config.jwt_access_secret,
+    config.jwt_access_expires_in as SignOptions,
+  );
+
+  const refreshToken = jwtUtils.creatToken(
+    jwtPayload,
+    config.jwt_refresh_secret,
+    config.jwt_refresh_expires_in as SignOptions,
+  );
+
+  return { accessToken, refreshToken };
 };
 
 export const authService = {
