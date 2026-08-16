5018ae1dd499d1f2d370d0532efefa422fbd08c8
JewelHFahim <jewelhfahim@gmail.com>
2026-08-16 23:05:08 +0600

    continue on premium

--- src/modules/premium/premium.controller.ts
+++ src/modules/premium/premium.controller.ts
@@ -9,6 +9,7 @@ import { premiumServices } from "./premium.service";
 const handletGetPremiumContent = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const result = await premiumServices.getPremiumContent();
+    console.log("result", result)
 
     sendResponse(res, {
       success: true,
